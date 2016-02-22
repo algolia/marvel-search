ba8e19943c1551c6ea459357c474a4fe51f92721
Pixelastic <tim@pixelastic.com>
2016-02-22 19:00:08 +0100

    feat(images): Add images to the consolidation process

--- test/helper-wikidata-test.js
+++ test/helper-wikidata-test.js
@@ -34,4 +34,86 @@ describe('HelperWikidata', () => {
       expect(actual).toEqual(false);
     });
   });
+
+  describe('getAliases', () => {
+    it('should return empty array if no entities', () => {
+      // Given
+      let input = {
+      };
+
+      // When
+      let actual = Helper.getAliases(input);
+
+      // Then
+      expect(actual).toEqual([]);
+    });
+
+    it('should return empty array if no aliases', () => {
+      // Given
+      let input = {
+        entities: {
+          something: {
+          }
+        }
+      };
+
+      // When
+      let actual = Helper.getAliases(input);
+
+      // Then
+      expect(actual).toEqual([]);
+    });
+
+    it('should return all the aliases', () => {
+      // Given
+      let input = {
+        entities: {
+          something: {
+            aliases: {
+              en: [{
+                value: 'Peter Parker'
+              }, {
+                value: 'Webhead'
+              }]
+            }
+          }
+        }
+      };
+
+      // When
+      let actual = Helper.getAliases(input);
+
+      // Then
+      expect(actual).toEqual(['Peter Parker', 'Webhead']);
+    });
+
+    it('should only return english aliases', () => {
+      // Given
+      let input = {
+        entities: {
+          something: {
+            aliases: {
+              fr: [{
+                language: 'fr',
+                value: 'Tisseur'
+              }],
+              en: [{
+                language: 'en',
+                value: 'Peter Parker'
+              }, {
+                language: 'en',
+                value: 'Webhead'
+              }]
+            }
+          }
+        }
+      };
+
+      // When
+      let actual = Helper.getAliases(input);
+
+      // Then
+      expect(actual).toEqual(['Peter Parker', 'Webhead']);
+    });
+  });
 });
