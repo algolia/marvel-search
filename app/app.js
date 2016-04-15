c61013d770ab9bf8818a1ddd725c1e721390dd13
Pixelastic <tim@pixelastic.com>
2016-04-15 13:10:43 +0200

    Cleanup sidebar

--- app/app.js
+++ app/app.js
@@ -16,7 +16,6 @@ let Marvel = {
     this.addHitsWidget();
     this.addPaginationWidget();
     this.addRefinementList();
-    this.renderHits();
 
     this.search.start();
   },
@@ -146,328 +145,166 @@ let Marvel = {
       })
     );
   },
-  renderHits(){
-    var renderedHits = {
-      render: function(options) {
-        processHeroProfile();
-      }
-    };
+  addHits() {
+    // this.search.addWidget(renderedHits);
+    // var renderedHits = {
+    //   render: function(options) {
+    //     processHeroProfile();
+    //   }
+    // };
+  },
+  // Apply luminance to a given hexadecimal color
+  colorLuminance(hex, lum) {
 
-    this.search.addWidget(renderedHits);
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+      hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+    }
+    lum = lum || 0;
+
+    // convert to decimal and change luminosity
+    var rgb = "#", c, i;
+    for (i = 0; i < 3; i++) {
+      c = parseInt(hex.substr(i*2,2), 16);
+      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+      rgb += ("00"+c).substr(c.length);
+    }
+
+    return rgb;
   }
 
 };
 
 export default Marvel;
 
-function processHeroProfile(){
-
-  var hit = $('.hit'),
-      results = $('.hits'),
-      profile = $('.hero-profile'),
-      profileHeader = $('.hero-profile header'),
-      profileName = $('.hero-profile .hero-name'),
-      profileRealName = $('.hero-profile .hero-secret-identity'),
-      profileAvatar = $('.hero-profile .hero-avatar img'),
-      profileDescription = $('.hero-profile .hero-description'),
-      profilePartners = $('.hero-profile .hero-partners'),
-      profileHeroVillain = $('.hero-profile .factions'),
-      profilePowers = $('.hero-profile .hero-powers'),
-
-      closeProfile = $('#closeProfile');
-
-      // Add dynamic style tag
-      profileHeader.prepend('<style id="dynamic-style" />');
-      var dynamicStyle = $('#dynamic-style');
-
-  // When a "view profile" button is pressed
-  hit.each(function(){
-    $(this).click(function(e,t){
-      e.preventDefault();
-      var $this = $(this);
-      var datas = $(this).closest('.hit').find('.dump').val(),
-          datas = $.parseJSON(datas);
-
-
-      // Fetch & define values
-      var
-      heroName = datas.name,
-      heroSecretId = datas.secretIdentities[0],
-      heroAvatar = datas.images.thumbnail || './default.jpg',
-      heroBanner = datas.images.banner,
-      heroBackground = datas.images.background || 'http://i.annihil.us/u/prod/marvel/i/mg/c/50/537bafe4149ad.gif',
-      heroDesc = datas.description,
-      heroPowers = datas.powers,
-      heroPartners = datas.partners,
-      isHero = datas.isHero,
-      isVillain = datas.isVillain;
-
-      if (_.has(datas, 'mainColor.hexa')) {
-        var heroColor = colorLuminance('#' + datas.mainColor.hexa, 1.5);
-      } else {
-        var heroColor = '#ccc'
-      }
-
-      // Appy them
-      profileName.html(heroName);
-      profileAvatar.attr('src', heroAvatar);
-      profileDescription.html('<p>'+heroDesc+'</p>');
-      profileRealName.html(heroSecretId);
-
-
-
-
-      // Add the proper colors
-      profileAvatar.parent().attr('style','border-color:'+ heroColor);
-      dynamicStyle.text('').text('.hero-profile header:after{background-color:'+heroColor+';}.hero-profile .hero-faction:before,.hero-profile .hero-faction:after{background: linear-gradient(to bottom, '+heroColor+', '+  colorLuminance(heroColor, 1.5)+') !important}')
-
-
-      // Give the profile header the proper images
-      profileHeader.attr('style','background-image:url(' + heroBackground + ');border-color:' + heroColor);
-
-      // Check if he is a hero, a vilain, both, or null
-      if(isHero && !isVillain) {
-        profileHeroVillain.attr({
-          'data-is-hero': 'true',
-          'data-is-villain': 'false'
-        })
-      }
-      if(isVillain && !isHero) {
-        profileHeroVillain.attr({
-          'data-is-hero': 'false',
-          'data-is-villain': 'true'
-        })
-      }
-      if(!isHero && !isVillain) {
-        profileHeroVillain.attr({
-          'data-is-hero': 'true',
-          'data-is-villain': 'true'
-        })
-      }
-      if(isHero===null && isVillain===null){
-        profileHeroVillain.attr({
-          'data-is-hero': 'false',
-          'data-is-villain': 'false'
-        })
-      }
-
-
-      // Loop & wrap all the powers
-      profilePowers.html('');
-      var power = '';
-      $.each(heroPowers, function (index, key){
-        power += "<span class='power'>"+key+"</span>";
-      });
-      profilePowers.append(power);
-
-      // Loop & wrap all the partners
-      profilePartners.html('');
-      var partner = '';
-      if(heroPartners==0){
-        partner += "Unknown"
-      } else {
-        $.each(heroPartners, function (index, key){
-          partner = "<span class='partner'>"+key+"</span>, ";
-        });
-      }
-      profilePartners.append(partner);
-
-
-      if(!results.hasClass('open')){
-        results.addClass('open')
-        profile.addClass('shown')
-      }
-    });
-  });
-
-  // When the "close profile" button is pressed
-  closeProfile.on('click', function(){
-    if(results.hasClass('open')){
-      results.removeClass('open')
-      profile.removeClass('shown')
-    }
-  });
-}
-
-
-function colorLuminance(hex, lum) {
-
-  // validate hex string
-  hex = String(hex).replace(/[^0-9a-f]/gi, '');
-  if (hex.length < 6) {
-    hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
-  }
-  lum = lum || 0;
-
-  // convert to decimal and change luminosity
-  var rgb = "#", c, i;
-  for (i = 0; i < 3; i++) {
-    c = parseInt(hex.substr(i*2,2), 16);
-    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-    rgb += ("00"+c).substr(c.length);
-  }
-
-  return rgb;
-}
-// function imageDimensions($url) {
-//   var u = $url;
-
-// }
-///////////////////////////
-/// Toggle hero-profile \\\
-///////////////////////////
 // function processHeroProfile(){
-//   var hit = document.querySelectorAll('.hit'),
-//       results = document.querySelector('.hits'),
-//       profile = document.querySelector('.hero-profile');
-//   for(var i=0;i<hit.length;i++){
-//     hit[i].addEventListener('click', function(){
-//       alert()
-//       if(!results.classList.contains('open')){
-//         results.classList.add('open')
-//         profile.classList.add('shown')
+// 
+//   var hit = $('.hit'),
+//       results = $('.hits'),
+//       profile = $('.hero-profile'),
+//       profileHeader = $('.hero-profile header'),
+//       profileName = $('.hero-profile .hero-name'),
+//       profileRealName = $('.hero-profile .hero-secret-identity'),
+//       profileAvatar = $('.hero-profile .hero-avatar img'),
+//       profileDescription = $('.hero-profile .hero-description'),
+//       profilePartners = $('.hero-profile .hero-partners'),
+//       profileHeroVillain = $('.hero-profile .factions'),
+//       profilePowers = $('.hero-profile .hero-powers'),
+// 
+//       closeProfile = $('#closeProfile');
+// 
+//       // Add dynamic style tag
+//       profileHeader.prepend('<style id="dynamic-style" />');
+//       var dynamicStyle = $('#dynamic-style');
+// 
+//   // When a "view profile" button is pressed
+//   hit.each(function(){
+//     $(this).click(function(e,t){
+//       e.preventDefault();
+//       var $this = $(this);
+//       var datas = $(this).closest('.hit').find('.dump').val(),
+//           datas = $.parseJSON(datas);
+// 
+// 
+//       // Fetch & define values
+//       var
+//       heroName = datas.name,
+//       heroSecretId = datas.secretIdentities[0],
+//       heroAvatar = datas.images.thumbnail || './default.jpg',
+//       heroBanner = datas.images.banner,
+//       heroBackground = datas.images.background || 'http://i.annihil.us/u/prod/marvel/i/mg/c/50/537bafe4149ad.gif',
+//       heroDesc = datas.description,
+//       heroPowers = datas.powers,
+//       heroPartners = datas.partners,
+//       isHero = datas.isHero,
+//       isVillain = datas.isVillain;
+// 
+//       if (_.has(datas, 'mainColor.hexa')) {
+//         var heroColor = colorLuminance('#' + datas.mainColor.hexa, 1.5);
 //       } else {
-//         results.classList.remove('open')
-//         profile.classList.remove('shown')
+//         var heroColor = '#ccc'
 //       }
-//     })
-//   }
-// }
-
-
-// window.onload = function(){
-//   processHeroProfile()
-// }
-
-
-
-
-
-
-// // Returns the Marvel image if available, otherwise the one from Wikipedia.
-// // Back to default if none is found
-// function getImage(record) {
-//   if (record.marvel && record.marvel.image) {
-//     return record.marvel.image;
-//   }
-//   if (record.image.url) {
-//     return record.image.url;
-//   }
-//   return 'https://pixelastic.github.io/marvel/default.jpg';
-// }
-//
-// // Returns Marvel description if found, list of powers otherwise
-// function getDescription(record) {
-//   // Si description on l'affiche
-//   // sinon on affiche le power text
-//   if (record.marvel && record.marvel.description) {
-//     if (record._highlightResult.marvel.description) {
-//       return record._highlightResult.marvel.description.value;
-//     }
-//     return record.marvel.description;
-//   }
-//   return record.powersText.join('<br>');
-// }
-//
-//
-// search.addWidget(
-//   instantsearch.widgets.hits({
-//     container: '#hits',
-//     hitsPerPage: 80,
-//     templates: {
-//       empty: noResultsTemplate,
-//       item: hitTemplate
-//     },
-//     transformData: {
-//       item: function (data) {
-//         if (data.creators) {
-//           data.creators = data.creators.join(', ');
-//         }
-//         if (data.species) {
-//           data.species = data.species.join(', ');
-//         }
-//
-//         data.description = getDescription(data);
-//
-//         // Use cloudinary to load smaller images
-//         var cloudinaryPrefix = 'http://res.cloudinary.com/pixelastic-marvel/image/fetch/h_190,q_100,c_scale,f_auto/';
-//         data.image = cloudinaryPrefix + getImage(data);
-//
-//         return data;
+// 
+//       // Appy them
+//       profileName.html(heroName);
+//       profileAvatar.attr('src', heroAvatar);
+//       profileDescription.html('<p>'+heroDesc+'</p>');
+//       profileRealName.html(heroSecretId);
+// 
+// 
+// 
+// 
+//       // Add the proper colors
+//       profileAvatar.parent().attr('style','border-color:'+ heroColor);
+//       dynamicStyle.text('').text('.hero-profile header:after{background-color:'+heroColor+';}.hero-profile .hero-faction:before,.hero-profile .hero-faction:after{background: linear-gradient(to bottom, '+heroColor+', '+  colorLuminance(heroColor, 1.5)+') !important}')
+// 
+// 
+//       // Give the profile header the proper images
+//       profileHeader.attr('style','background-image:url(' + heroBackground + ');border-color:' + heroColor);
+// 
+//       // Check if he is a hero, a vilain, both, or null
+//       if(isHero && !isVillain) {
+//         profileHeroVillain.attr({
+//           'data-is-hero': 'true',
+//           'data-is-villain': 'false'
+//         })
 //       }
+//       if(isVillain && !isHero) {
+//         profileHeroVillain.attr({
+//           'data-is-hero': 'false',
+//           'data-is-villain': 'true'
+//         })
+//       }
+//       if(!isHero && !isVillain) {
+//         profileHeroVillain.attr({
+//           'data-is-hero': 'true',
+//           'data-is-villain': 'true'
+//         })
+//       }
+//       if(isHero===null && isVillain===null){
+//         profileHeroVillain.attr({
+//           'data-is-hero': 'false',
+//           'data-is-villain': 'false'
+//         })
+//       }
+// 
+// 
+//       // Loop & wrap all the powers
+//       profilePowers.html('');
+//       var power = '';
+//       $.each(heroPowers, function (index, key){
+//         power += "<span class='power'>"+key+"</span>";
+//       });
+//       profilePowers.append(power);
+// 
+//       // Loop & wrap all the partners
+//       profilePartners.html('');
+//       var partner = '';
+//       if(heroPartners==0){
+//         partner += "Unknown"
+//       } else {
+//         $.each(heroPartners, function (index, key){
+//           partner = "<span class='partner'>"+key+"</span>, ";
+//         });
+//       }
+//       profilePartners.append(partner);
+// 
+// 
+//       if(!results.hasClass('open')){
+//         results.addClass('open')
+//         profile.addClass('shown')
+//       }
+//     });
+//   });
+// 
+//   // When the "close profile" button is pressed
+//   closeProfile.on('click', function(){
+//     if(results.hasClass('open')){
+//       results.removeClass('open')
+//       profile.removeClass('shown')
 //     }
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.stats({
-//     container: '#stats'
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#creators',
-//     attributeName: 'creators',
-//     operator: 'and',
-//     limit: 10
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#teams',
-//     attributeName: 'teams',
-//     operator: 'and',
-//     limit: 8
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#species',
-//     attributeName: 'species',
-//     operator: 'and',
-//     limit: 5
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.refinementList({
-//     container: '#powers',
-//     attributeName: 'powers',
-//     operator: 'and',
-//     limit: 8
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.pagination({
-//     container: '#pagination',
-//     cssClasses: {
-//       active: 'active'
-//     },
-//     labels: {
-//       previous: '<i class="fa fa-angle-left fa-2x"></i> Previous page',
-//       next: 'Next page <i class="fa fa-angle-right fa-2x"></i>'
-//     },
-//     showFirstLast: false
-//   })
-// );
-//
-// search.addWidget(
-//   instantsearch.widgets.clearAll({
-//     container: '#clear-all',
-//     templates: {
-//       link: '<i class="fa fa-eraser"></i> Clear all filters'
-//     },
-//     cssClasses: {
-//       root: 'btn btn-block btn-default'
-//     },
-//     autoHideContainer: true
-//   })
-// );
-//
-// search.start();
-//
-// $('#q').focus();
+//   });
+// }
+
+
